6d6a2d5823f4355b995b92284022ebafbdd9ca7c
Zoltan Krizsan <krizsan@iit.uni-miskolc.hu>
2021-03-24 00:02:47 +0100

    test

---
+++ 2_first_steps/gradient.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+def calculate_gradient():
+    x = tf.Variable(3.0)
+
+    # https://docs.python.org/3/library/operator.html
+    with tf.GradientTape() as tape:
+        y = x**2  # pow(x, 2)
+
+    # dy = (3*3) - 3 = 9 - 3
+    return tape.gradient(y, x)
+
+
+if "__name__" == "__main__":
+    dy_dx = calculate_gradient()
+    print(dy_dx.numpy())
--- 2_first_steps/tests/test_gradient.py
+++ 2_first_steps/tests/test_gradient.py
@@ -1,16 +1,21 @@
-import tensorflow as tf
-
-from src.gradient import dy_dx
-
-
-class GradientTest(tf.test.TestCase):
-
-    def setUp(self):
-        super(tf.test.TestCase, self).setUp()
-
-    def test_gradient(self):
-        self.assertEqual(dy_dx, 6)
+from unittest.mock import patch, MagicMock
+from unittest.case import TestCase
+from unittest import mock
+from gradient import calculate_gradient
 
 
-# tf.test.main()
+class GradientTest(TestCase):
 
+    @mock.patch('gradient.tf')
+    def test_calculate_gradient(self, mock_tf):
+        gradient_return_value = 123
+        mock_tf.GradientTape = MagicMock()
+        mock_tf.GradientTape.gradient = MagicMock()
+        mock_tf.GradientTape.gradient.return_value = gradient_return_value
+        dy_dx = calculate_gradient()
+        self.assertEqual(dy_dx, dy_dx)
+        mock_tf.GradientTape.assert_called_once()
+        mock_tf.GradientTape.gradient.assert_called_once()
+
+if __name__ == '__main__':
+    unittest.main()
